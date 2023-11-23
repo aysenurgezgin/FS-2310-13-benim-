d75cea1fa673275b8f30de9d8a65e08147ed88dc
aysenurgezgin <aysenurgezgin99@gmail.com>
2023-11-23 21:57:45 +0300

    ödeb-ve bak

--- Section-04-Javascript/Week-06/23-11-2023/19-Calculator/app.js
+++ Section-04-Javascript/Week-06/23-11-2023/19-Calculator/app.js
@@ -1,39 +1,33 @@
 const display = document.querySelector(".calculator-input");
 const keys = document.querySelector(".calculator-keys");
 
-let displayValue="0";
-// her yerde kullacağım için bunu bu kısımda değişken atadık(aşağıdaki)
-let waitingForSecondValue= false;  
-// sen normal çalışıyorsun demek üstteki
-let previousValue= null;
-// yakalama icinn yazılan
-let operator=null;
-// içinde tutacağımız 
+let displayValue = "0";
+let waitingForSecondValue = false;
+let previousValue = null;
+let operator = null;
 
-
-function updateDisplay(){
-    display.value=displayValue;
+function updateDisplay() {
+    display.value = displayValue;
 }
 
-keys.addEventListener("click",function(e){
-    const element=e.target;
-    const value=element.value;
-    if(!element.matches("button")) return;
+keys.addEventListener("click", function (e) {
+    const element = e.target;
+    const value = element.value;
+    if (!element.matches("button")) return;
     switch (value) {
         case "+":
         case "-":
-        case"*":
-        case"/":
-        case"=":
+        case "*":
+        case "/":
+        case "=":
             handleOperator(value);
             break;
-        case".":
+        case ".":
             inputDecimal();
             break;
-        case"clear":
+        case "clear":
             clearDisplay();
             break;
-
         default:
             inputNumber(value);
             break;
@@ -41,17 +35,60 @@ keys.addEventListener("click",function(e){
     updateDisplay();
 });
 
+function handleOperator(nextOperator) {
+    let value = parseFloat(displayValue);
+    if (operator != null && waitingForSecondValue == true) {
+        operator = nextOperator;
+       
+        return;
+    }
 
-// ilk olarak sayı girişini atayacağımız için burada başladık
-function handleOperator(nextOperator){
-    waitingForSecondValue=true;
+    if (previousValue == null) {
+        previousValue = value;
+    } else {
+        const result = calculate(previousValue, operator, value);
+        displayValue = result.toString();
+        previousValue = result;
+    }
+    operator = nextOperator;
+    waitingForSecondValue = true;
+    
 }
 
-function inputNumber(num){
-    if(!waitingForSecondValue){ 
-   displayValue =displayValue=="0"? num: displayValue + num;
-    }else{
-        displayValue=num;
+function calculate(firstNumber, op, secondNumber) {
+    switch (op) {
+        case "+":
+            return firstNumber + secondNumber;
+        case "-":
+            return firstNumber - secondNumber;
+        case "*":
+            return firstNumber * secondNumber;
+        case "/":
+            return firstNumber / secondNumber;
+        default:
+            return secondNumber;
+    }
+}
+
+function inputDecimal() {
+    if (displayValue.indexOf(".") == -1) displayValue += ".";
+}
+
+function clearDisplay() {
+    displayValue = "0";
+    waitingForSecondValue = false;
+    previousValue = null;
+    operator = null;
+}
+
+function inputNumber(num) {
+    if (!waitingForSecondValue) {
+        displayValue = displayValue == "0" ? num : displayValue + num;
+    } else {
+        displayValue = num;
+        waitingForSecondValue = false;
     }
 }
 updateDisplay();
+// hesap makinesinei kurcala
+// consolda bak bakalım neede hata yaptığını  (tayfun erbilen çok iyi js bilgisi var)PROTOTURK kanalı
