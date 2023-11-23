5e0ae985cb7c711cc6575355838740b3f13aa23b
aysenurgezgin <aysenurgezgin99@gmail.com>
2023-11-23 21:02:19 +0300

    aa

--- Section-04-Javascript/Week-06/22-11-2023/19-Calculator/app.js
+++ Section-04-Javascript/Week-06/22-11-2023/19-Calculator/app.js
@@ -44,14 +44,60 @@ keys.addEventListener("click",function(e){
 
 // ilk olarak sayı girişini atayacağımız için burada başladık
 function handleOperator(nextOperator){
+    let value=parseFloat(displayValue);
+    
+    if(operator==null && waitingForSecondValue==true){
+        operator=nextOperator;
+   
+        return;
+    }
+    // eşittire bastığımız zaman bu kısımda olmasını bekliyoruz
+    if(previousValue==null){
+        previousValue=value;
+    }else{
+      const result=  calculate(previousValue, operator, value);
+     displayValue=result.toString();
+     previousValue=result;
+    }
+    operator=nextOperator;
     waitingForSecondValue=true;
+};
+function calculate(firstNumber, op, secondNumber){
+    // küçük kod yazmam gerktiğinde switc için op u kontrol etmek için
+    // console.log("operator",operator)
+    switch(op){
+        case"+":
+        return firstNumber+secondNumber;
+        case "-":
+            return firstNumber - secondNumber;
+        case "*":
+            return firstNumber * secondNumber;
+        case "/":
+            return firstNumber / secondNumber;
+        default:
+            return secondNumber;
+    }
 }
 
+function inputDecimal(){
+    // eksi bir olması için de yok demek yoksa onu denemek istiyorus
+  if(displayValue.indexOf(".")==-1) displayValue+=".";
+};
+function clearDisplay(){
+    displayValue= "0";
+    waitingForSecondValue=false;
+    previousValue=null;
+    operator=null;
+};
+
+// kontrol fonsiyonun olduğu nokta bu kısım
 function inputNumber(num){
     if(!waitingForSecondValue){ 
-   displayValue =displayValue=="0"? num: displayValue + num;
+        displayValue =displayValue=="0"? num: displayValue + num;
     }else{
+        // sayı girdiğimiz kısım ya ilk ya operetore sayı girdiğimiz zaman burası çalışacak
         displayValue=num;
+        waitingForSecondValue=false;
     }
 }
 updateDisplay();
