2a60c233ccd7095579d5ca97e04f6699c18d033f
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-01-07 14:58:51 +0300

    hocanın reposundan çekersin

---
+++ Section-09-MVC/Week-12/07-01-2024/MiniShop/MiniShop.Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CategoryProduct> CategoryProducts { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-LHV5FJ2\SQLEXPRESS;Database=MiniShopDb;user=sa; password=1234;TrustServerCertificate=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CategoryProduct>().HasKey(x => new 
+            {x.CategoryId,}
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
