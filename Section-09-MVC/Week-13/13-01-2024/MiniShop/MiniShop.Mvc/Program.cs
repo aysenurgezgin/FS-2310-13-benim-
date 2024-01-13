adb2e23c9e915ab4c1108934caa39e43eb3cb86d
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-01-13 14:47:59 +0300

    data table işlemi yapıldı

--- Section-09-MVC/Week-13/13-01-2024/MiniShop/MiniShop.Mvc/Program.cs
+++ Section-09-MVC/Week-13/13-01-2024/MiniShop/MiniShop.Mvc/Program.cs
@@ -30,8 +30,15 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapAreaControllerRoute(
+    name:"Admin",
+    areaName:"Admin",
+    pattern:"admin/{controller=Home}/{action=Index}/{id?}"
+    );
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
