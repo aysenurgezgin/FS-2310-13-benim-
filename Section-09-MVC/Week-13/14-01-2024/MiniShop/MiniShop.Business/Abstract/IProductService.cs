2f48c4e69a059980f2bbb7ba3544108e40e20015
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-01-14 09:58:13 +0300

    async yapılandırma yapma mantığı 09::58

---
+++ Section-09-MVC/Week-13/14-01-2024/MiniShop/MiniShop.Business/Abstract/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using MiniShop.Core.ViewModels;
+using MiniShop.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Business.Abstract
+{
+    public interface IProductService
+    {
+        //Crud işlemlerini gerçekleitirecek metot imzalarını yazacağız.
+        void Create(ProductViewModel model); //create(c)
+        List<ProductViewModel> GetAll(bool? isHome= null, bool? isActive=null, bool? isDelete=null); //(r)read
+        ProductViewModel GetById(int id);
+       
+        void Update(ProductViewModel model);//(u)pdate
+        void HardDelete(int id);//(d)elete
+       
+        void SoftDelete(int id);
+
+
+
+    }
+}
