2671190b9cba9452ab7f71e47278995f9bf1f02f
Ayse Nur Gezgin <aysenurgezgin99@gmail.com>
2024-01-20 19:04:05 +0300

    dosya ekleme

---
+++ Section-09-MVC/Week-14/20-01-2024/MiniShop/MiniShop.API/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+using MiniShop.Business.Concrete;
+
+namespace MiniShop.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryManager;
+
+        public CategoriesController(ICategoryService categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+        var response = await _categoryManager.GetAllAsync();
+            return null;
+        }
+    }
+}
+
+
+//Burada eksik var GetById i yap CategoryManeger içndekileylşe birlikte
---
+++ Section-09-MVC/Week-14/20-01-2024/MiniShop/MiniShop.Shared/ResponseDTOs/Response.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ResponseDTOs
+{
+    public  class Response<T>
+    {
+
+        public T Data { get; set; }
+        public List<string> Errors { get; set; }
+        public int StatusCode { get; set; }
+        public bool IsSucceeded { get; set; }
+        /// <summary>
+        /// Bu methot , yapılan işlemin başarlılı old.zamnalarda başarılı bir cevapla birlikte üretilen datayı  ve status codu  döndürmek için kullanıllır.
+        /// </summary>
+        /// <param name="data">Geri döndürülecek veri</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod(200,201 vb)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T>
+            {
+                Data = data,
+                StatusCode = statusCode,
+                IsSucceeded = true
+            };
+         }
+        /// <summary>
+        /// Bu methot işlemin başarlıl old.ifade eden bir status code döndürmek için kullanılır
+        /// </summary>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod(200,201 vb)</param>
+        /// <returns>Response<typeparamref name="T"/></returns>
+        public static Response<T>Success(DTOs.CategoryDTO categoryDtoList, int statusCode)
+        {
+            return new Response<T>
+            {
+                Data =default(T),
+                StatusCode = statusCode,
+                IsSucceeded = true
+
+            };
+        }
+        /// <summary>
+        /// Bu methot geriye başarısız bir cevap olarak içinde bir hata olamhata listesini döndürmek için kullanılır.
+        /// </summary>
+        /// <param name="error">Hata listesi</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod(200,201 vb)</param>
+        /// <returns>Response <typeparamref name="T"/></returns>
+        /// 
+        public static Response<T> Fail(List<string> errors, int statusCode) 
+        {
+            return new Response<T>
+            {
+                Errors = errors,
+                StatusCode = statusCode,
+                IsSucceeded = false
+            };
+        }
+   
+        /// <summary>
+        /// Bu methot geriye başarısız bir cevap olarak içinde bir hata olamhata listesini döndürmek için kullanılır.
+        /// </summary>
+        /// <param name="error">Hata metni</param>
+        /// <param name="statusCode">İşlemin sonucunu ifade eden kod(200,201 vb)</param>
+        /// <returns>Response <typeparamref name="T"/></returns>
+        public static Response<T> Fail(string error, int statusCode)
+        {
+            return new Response<T>
+            {
+                Errors = new List<string> { error },
+                StatusCode = statusCode,
+                IsSucceeded = false
+            };
+        }
+
+    }  
+}
