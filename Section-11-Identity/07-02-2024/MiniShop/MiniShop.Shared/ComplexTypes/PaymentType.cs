01122698da952d0649f8ab84656d8c93b5394e71
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-02-07 21:42:07 +0300

    a

---
+++ Section-11-Identity/07-02-2024/MiniShop/MiniShop.Shared/ComplexTypes/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ComplexTypes
+{
+    public enum PaymentType
+    {
+        CreditCard=0,
+        Eft=1
+    }
+}
