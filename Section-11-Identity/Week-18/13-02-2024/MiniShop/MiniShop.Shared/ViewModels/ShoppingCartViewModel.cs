6dfd687a6df704e8193c8105f7dbb28ac73d7711
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-02-13 21:27:08 +0300

    ürünler

---
+++ Section-11-Identity/Week-18/13-02-2024/MiniShop/MiniShop.Shared/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniShop.Shared.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public int ShoppingCartId { get; set; }
+        public List<ShoppingCartItemViewModel> Items { get; set; }
+        public double TotalPrice()
+        {
+            return Items
+        }
+    }
+}
