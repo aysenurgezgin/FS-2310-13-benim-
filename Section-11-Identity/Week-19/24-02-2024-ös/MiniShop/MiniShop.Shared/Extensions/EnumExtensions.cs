a71588d0aa7fc65c178d7df0c9fde876ea9c7788
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-02-24 13:26:46 +0300

    mapping gibi mapster var bakarsın onadaü

---
+++ Section-11-Identity/Week-19/24-02-2024-ös/MiniShop/MiniShop.Shared/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MiniShop.Shared.Extensions
+
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value) 
+        {
+            string result =value
+                .GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()//Null değilse oluypr bu sonraki soru işareti
+                ?.GetName();
+            return result;
+        }
+    }
+}
---
+++ Section-11-Identity/Week-19/24-02-2024/MiniShop/MiniShop.UI/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MiniShop.Business.Abstract;
+
+namespace MiniShop.UI.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "SuperAdmin, Admin")]
+    [Area("Admin")]
+
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderManager;
+
+        public OrderController(IOrderService orderManager)
+        {
+            _orderManager = orderManager;
+        }
+
+        public async Task <IActionResult> Index()
+        {
+            var orders =await _orderManager.GetOrdersAsync();
+          
+            return View(orders);
+        }
+    }
+}
