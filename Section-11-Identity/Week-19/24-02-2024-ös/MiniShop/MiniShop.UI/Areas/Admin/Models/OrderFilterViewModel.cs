fa989d4ddda5561a388cb4153612cd3dd727f511
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-02-24 14:38:47 +0300

    hocanın reposundan güncelle neden se server da çaışltıramadım

---
+++ Section-11-Identity/Week-19/24-02-2024-ös/MiniShop/MiniShop.UI/Areas/Admin/Models/OrderFilterViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using MiniShop.Shared.ViewModels;
+
+namespace MiniShop.UI.Areas.Admin.Models
+{
+    public class OrderFilterViewModel
+    {
+        public List<AdminOrderViewModel> Products { get;  set; }
+        public List<SelectListItem> ProductListItems { get;  set; }
+    }
+}
