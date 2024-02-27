21a789d10d5bc6d2649b73bd6a4000c0bc1eecd8
aysenurgezgin <aysenurgezgin99@gmail.com>
2024-02-27 21:29:28 +0300

    bakarsın

---
+++ Section-11-Identity/Week-20/27-02-2024/MiniShop/MiniShop.Shared/Extensions/EnumExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace MiniShop.Shared.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            string result = value
+                .GetType()
+                .GetMember(value.ToString())
+                .First()
+                .GetCustomAttribute<DisplayAttribute>()
+                ?.GetName();
+            return result;
+        }
+        //Benim yaptığım enum yapısı;
+        //public static string changeIsState(this Enum value)
+        //{
+        //    string result = value
+        //        .StateName();
+        //    return ViewModels();
+        //}
+
+        //private static string ViewModels()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
